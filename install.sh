eef78f4e713ff5c7dd4e654e978b29d9860a3a5b
Rodrigo Domingues da Silva <rodrigodominguessilva@gmail.com>
2024-02-15 21:20:39 -0300

    adicionando opção na instalação de não rodar o arquivo de configuração

--- install.sh
+++ install.sh
@@ -14,7 +14,15 @@ if [ $? -eq 0 ]; then
     sudo chmod +x /usr/bin/sync-hd-monitor.sh
     sudo chmod +x /usr/bin/sync-hd.sh
     sudo chmod +x /usr/bin/sync-config.sh
-    bash sync-config.sh
+    echo Deseja iniciar o script de configuração?
+    echo para sim digite: s
+    read input
+    if [ "$input" ="s" ];then
+        bash sync-config.sh
+    else 
+        echo Para iniciar o script de configuração em um outro momento execute:
+        echo bash sync-config.sh
+    fi
 else
     echo "Senha do sudo incorreta. Instalação cancelada."
 fi
